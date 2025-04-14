dd74dfac1a35c3ddd831c9c5abaa649888a17d72
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-04-14 23:32:20 +0900

    Update: ArticleDetailおよびHomeコンポーネントにdynamicエクスポートを追加

--- packages/frontend/src/app/articles/[slug]/page.tsx
+++ packages/frontend/src/app/articles/[slug]/page.tsx
@@ -2,6 +2,8 @@ import DeleteButton from "@/app/articles/_components/DeleteButton";
 import { getArticle } from "@/services/articles";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic'
+
 export default async function ArticleDetail({
   params,
 }: {
--- packages/frontend/src/app/articles/page.tsx
+++ packages/frontend/src/app/articles/page.tsx
@@ -1,6 +1,8 @@
 import { getArticles } from "@/services/articles";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const { data: articles } = await getArticles();
 
